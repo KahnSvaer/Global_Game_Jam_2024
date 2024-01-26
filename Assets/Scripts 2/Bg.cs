55c71a52de281e146a7c17a0135fbae6cc23ae8f
LAPTOP-HABOT7VT\Sushant <sushantsudan3075@gmail.com>
2024-01-27 04:38:41 +0530

    Some changes to scene

---
+++ Assets/Scripts 2/Bg.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Bg : MonoBehaviour
+{
+    public float lenght , startPos;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    void Start()
+    {
+        startPos = transform.position.x;
+        lenght = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+    void FixedUpdate()
+    {
+        float dist = cam.transform.position.x * parallaxEffect;
+        transform.position = new Vector3(startPos+dist , transform.position.y,transform.position.z); 
+    }
+
+}
