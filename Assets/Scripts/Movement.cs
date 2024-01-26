3b60d5f051e8419ffacb0e3b92c2bb26f2cd6af8
Shivansh Pachnanda <Shivansh.pachnanda1@gmail.com>
2024-01-26 22:33:03 +0530

    Added Horizontal Movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float horizontalSpeed = 3000f;
+    [SerializeField] float verticalSpeed = 50f;
+
+    float horizontalInput;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LeftRightMovement();
+        Jump();
+    }
+
+    private void LeftRightMovement()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        Debug.Log(horizontalInput);
+        float horizontalVelocity =  horizontalInput * Time.deltaTime * horizontalSpeed;
+        rb.velocity = new Vector2(horizontalVelocity ,rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        throw new NotImplementedException();
+    }
+}
