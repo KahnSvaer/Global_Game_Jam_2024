a66ca2b20a2cd13ecb65228a52a02e13892cc6e3
LAPTOP-HABOT7VT\Sushant <sushantsudan3075@gmail.com>
2024-01-27 02:41:47 +0530

    Initial animation and pizza and red bull too

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Movement : MonoBehaviour
 {
+    [SerializeField] Animator anim;
     [SerializeField] float horizontalSpeed = 3000f;
     [SerializeField] float verticalThrust = 150f;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundLayer;
 
     float horizontalInput;
-    public float HorizontalInput{get{return horizontalInput;}}
 
     bool isVerticalInput;
-    public bool IsVerticalInput{get{return isVerticalInput;}}
+    
 
     bool onGround;
     public bool OnGround{get{return onGround;}}
@@ -28,6 +30,7 @@ public class Movement : MonoBehaviour
     {
         LeftRightMovement();
         Jump();
+        GroundCheck();
     }
 
     private void LeftRightMovement()
@@ -35,6 +38,17 @@ public class Movement : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         float horizontalVelocity =  horizontalInput * Time.deltaTime * horizontalSpeed;
         rb.velocity = new Vector2(horizontalVelocity, rb.velocity.y);
+        if(Mathf.Abs(horizontalInput)>0.2f ){
+            if (onGround)
+            {
+                anim.Play("Walk2");
+            }else {
+                anim.Play("Idle");
+            }
+            ChangeDirection(horizontalInput/Mathf.Abs(horizontalInput));
+        }else{
+            anim.Play("Idle");
+        }
     }
 
     private void Jump()
@@ -44,11 +58,28 @@ public class Movement : MonoBehaviour
         {
             float forceY =  verticalThrust;
             rb.AddForce(new Vector2(0, forceY));
-            onGround = false;
+            
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D other) {
-        onGround = true;
+    void GroundCheck(){
+        if(Physics2D.Raycast(groundCheck.position,Vector2.down,0.1f,groundLayer)){
+            onGround = true;
+        }else{
+            onGround = false;
+        }
+
+    }
+    public void ChangeDirection(float set){
+        Vector3 tempScale = transform.localScale ;
+        tempScale.x = set;
+        transform.localScale = tempScale;
+
+    }
+    void OnTriggerEnter2D(Collider2D target){
+        if(target.tag == "torso"){
+            Debug.Log("0");
+        }
+        
     }
 }
